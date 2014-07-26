255e8d235df3dbc91a17d00f304be950a012eb86
Varun Naik <vcnaik94@gmail.com>
2014-07-26 00:55:23 -0700

    Started Git configurations

---
+++ Git/generateAliases.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git config --global alias.stage add
+git config --global alias.unstage 'reset HEAD --'
+
---
+++ Git/resetAliases.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git config --global --unset alias.stage
+git config --global --unset alias.unstage
+
