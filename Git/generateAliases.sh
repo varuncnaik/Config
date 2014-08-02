6e40355acffb9f53b65e9cda7f364910888307c8
Varun Naik <vcnaik94@gmail.com>
2014-08-02 10:14:48 -0700

    Improved call to git unstage inside nested directory

--- Git/generateAliases.sh
+++ Git/generateAliases.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 chmod u+x $DIR/git-vcn-unstage.sh
 cp $DIR/git-vcn-unstage.sh /usr/local/bin/git-vcn-unstage.sh
-git config --global alias.unstage '!git-vcn-unstage.sh'
+git config --global alias.unstage '!cd ${GIT_PREFIX:-.} && git-vcn-unstage.sh'
 
