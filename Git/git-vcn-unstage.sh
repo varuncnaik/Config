94bf0dee46f250ea0c13d1f1b613cdae13cc1dbe
Varun Naik <vcnaik94@gmail.com>
2014-08-06 20:47:35 -0700

    Refactoring, better output for unstage

--- Git/git-vcn-unstage.sh
+++ Git/git-vcn-unstage.sh
@@ -19,13 +19,19 @@ unstageIfNoConflict() {
     local line=$1
     local status=${line:0:2}
     local file=${line:3}
+    if [ "$status" == "??" ]; then
+        return 1
+    fi
     git reset HEAD -- $file > /dev/null
+    return 0
 }
 
 OPTIND=1 # Reset in case getopts was used previously in the script
 
+# Boolean globals
 ALL=0
 VERBOSITY=0
+FILE_UNSTAGED=0
 
 while getopts ":Av" opt; do
     case "$opt" in
@@ -41,43 +47,33 @@ done
 
 shift "$((OPTIND-1))"
 
-if ((ALL == 1)); then
-    if ((VERBOSITY==1)); then
-        echo "git reset"
-        echo "Unstaged changes:"
-    fi
+if ((ALL==1)); then
     lines=$(cd ${GIT_PREFIX:-.} && git status --porcelain)
-    if [ -z "$lines" ]; then
-        echo "No files unstaged."
-    else
-        while read line; do
-            unstageIfNoConflict "$line"
-            if ((VERBOSITY==1)); then
-                echo "$line"
-            fi
-        done <<<"$lines"
-    fi
-
+    verboseMsg="git reset"
 else
     if [ -z "$*" ]; then
         noChanges
-    else
-        if ((VERBOSITY==1)); then
-            echo "git reset HEAD -- $*"
-            echo "Unstaged changes:"
-        fi
-        lines=$(cd ${GIT_PREFIX:-.} && git status --porcelain -- $*)
-        if [ -z "$lines" ]; then
-            echo "No files unstaged."
-        else
-            while read line; do
-                echo "asdf"
-                unstageIfNoConflict "$line"
-                if ((VERBOSITY==1)); then
-                    echo "$line"
-                fi
-            done <<<"$lines"
-        fi
+        exit 1
     fi
+    lines=$(cd ${GIT_PREFIX:-.} && git status --porcelain -- $*)
+    verboseMsg="git reset HEAD -- $*"
+fi
+
+if ((VERBOSITY==1)); then
+    echo "$verboseMsg"
+    echo "Unstaged changes:"
+fi
+while IFS='' read -r line; do
+    unstageIfNoConflict "$line"
+    status=$?        # return value of the previous function
+    if ((status==0)); then
+        if ((VERBOSITY==1)); then
+            echo "$line"
+        fi
+        FILE_UNSTAGED=1
+    fi
+done <<<"$lines"
+if ((FILE_UNSTAGED==0)); then
+    echo "No changes made to index."
 fi
 
