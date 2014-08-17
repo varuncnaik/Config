475b8ab8d85cbfae28e16f6086153c33f52fc45c
Varun Naik <vcnaik94@gmail.com>
2014-08-17 16:39:08 -0700

    Set push.default to simple

--- Git/generateAliases.sh
+++ Git/generateAliases.sh
@@ -5,4 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod u+x $DIR/git-vcn-unstage.sh
 cp $DIR/git-vcn-unstage.sh /usr/local/bin/git-vcn-unstage.sh
 git config --global alias.unstage '!git-vcn-unstage.sh'
+git config --global push.default simple
 
