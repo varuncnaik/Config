48ba57cefb1f466e521f7b91934022dda02a096e
Varun Naik <vcnaik94@gmail.com>
2014-08-17 16:50:29 -0700

    Created word-diff alias

--- Git/generateAliases.sh
+++ Git/generateAliases.sh
@@ -5,5 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod u+x $DIR/git-vcn-unstage.sh
 cp $DIR/git-vcn-unstage.sh /usr/local/bin/git-vcn-unstage.sh
 git config --global alias.unstage '!git-vcn-unstage.sh'
+git config --global alias.word-diff 'diff --word-diff-regex="[^[:space:](),]+"'
+git config --global color.ui always
 git config --global push.default simple
 
--- Git/resetAliases.sh
+++ Git/resetAliases.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+git config --global --unset alias.word-diff
 git config --global --unset alias.unstage
 rm -rf /usr/local/bin/git-vcn-unstage.sh
 
