8a384cac43d8b51ec2a77724586d0fb42536c96c
Varun Naik <vcnaik94@gmail.com>
2014-08-31 16:37:45 -0700

    Commenting

--- Git/generateAliases.sh
+++ Git/generateAliases.sh
@@ -2,10 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Aliases
 chmod u+x $DIR/git-vcn-unstage.sh
 cp $DIR/git-vcn-unstage.sh /usr/local/bin/git-vcn-unstage.sh
 git config --global alias.unstage '!git-vcn-unstage.sh'
 git config --global alias.word-diff 'diff --word-diff-regex="[^[:space:](),]+"'
+
+# Always show color
 git config --global color.ui always
+
+# Simple push - push current branch to a branch of the same name
 git config --global push.default simple
 
